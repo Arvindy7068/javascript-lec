0fa82bdad7848bb60d4d6619c9100850e12f7b85
Arvind Yadav <arvindy7068@gmail.com>
2023-05-17 19:46:23 +0530

    Lecture-1

---
+++ interaction.js
@@ -0,0 +1,7 @@
+// let username = prompt("Enter Your Name");
+// console.log(username);
+
+
+
+let isDelete = confirm("Are you sure, you want to delete this user?")
+console.log(isDelete);
---
+++ main.js
@@ -0,0 +1,6 @@
+console.log('javascript')
+console.log('again printing')
+
+
+alert("hello");
+[1,2].forEach(alert);
